ce8cf95e0865b22794a92e1fc1b86391e39411c3
Spider9480 <omgawandeofficial9834899149@gmail.com>
2022-12-07 20:21:02 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import Homepage from './Views/Homepage';
+import React from "react";
+import Footer from "./Components/Footer/index";
+
+import Homepage from "./Views/Homepage";
 import MovieDetails from "./Views/MovieDetails";
 
 import { Routes, Route } from "react-router-dom";
-import { useEffect } from 'react';
-import SearchResults from './Views/SearchResults';
-
+import { useEffect } from "react";
+import SearchResults from "./Views/SearchResults";
 
 function App() {
   useEffect(() => {
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/watch" element={<MovieDetails />} />
         <Route exact path="/search/:query" element={<SearchResults />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./styles.module.scss";
+
+export default function Footer({ style }) {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div className={styles.footerContainer}>
+        <div>
+          <img
+            src="/logo.png"
+            alt="logo"
+            className={styles.logo}
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+        </div>
+        <h2 className={styles.text}>
+          We do not host any of the content served.{" "}
+        </h2>
+        <a
+          href="https://github.com/Swanand01/streaminal-tv"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src="/github.png" alt="github" className={styles.github} />
+        </a>
+      </div>
+      <div className={styles.footerCopyrights}>© Streminal Tv</div>
+    </>
+  );
+}
